21d78df8f69292f57634a74f20713b67712fe28e
Nisrinahumaira01 <114343691+Nisrinahumaira01@users.noreply.github.com>
2023-01-21 01:48:50 +0700

    Route CRUD Rekam medis & Visit

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,38 @@ Route::get('/approve/{id}',[AdminController::class,'approve']);
 Route::get('/canceled/{id}',[AdminController::class,'canceled']);
 Route::get('/delete_appoint/{id}',[AdminController::class,'delete_appoint']);
 Route::get('/appointexcel',[AdminController::class,'appointexcel']);
+
+//CRUD Kamar
+Route::get('/addkamar',[AdminController::class,'addkamar']);
+Route::post('/upload_kamar',[AdminController::class,'upload_kamar']);
+Route::get('/showkamar',[AdminController::class,'showkamar']);
+Route::get('/del_kamar/{id}',[AdminController::class,'del_kamar']);
+Route::get('/updatekamar/{id}',[AdminController::class,'updatekamar']);
+Route::post('/editkamar/{id}',[AdminController::class,'editkamar']);
+Route::get('/kamarexcel',[AdminController::class,'kamarexcel']);
+
+//CRUD rawat inap
+Route::get('/addrawatin',[AdminController::class,'addrawatin']);
+Route::post('/upload_rawat',[AdminController::class,'upload_rawat']);
+Route::get('/showrawatin',[AdminController::class,'showrawatin']);
+Route::get('/del_pasien/{id}',[AdminController::class,'del_pasien']);
+Route::get('/updatepasien/{id}',[AdminController::class,'updatepasien']);
+Route::post('/editpasien/{id}',[AdminController::class,'editpasien']);
+Route::get('/rawatinexcel',[AdminController::class,'rawatinexcel']);
+
+//CRUD Visit
+Route::get('/inputvisit',[AdminController::class,'inputvisit']);
+Route::post('/upload_visit',[AdminController::class,'upload_visit']);
+Route::get('/showvisit',[AdminController::class,'showvisit']);
+Route::get('/del_visit/{id}',[AdminController::class,'del_visit']);
+Route::get('/updatevisit/{id}',[AdminController::class,'updatevisit']);
+Route::post('/editvisit/{id}',[AdminController::class,'editvisit']);
+Route::get('/visitexcel',[AdminController::class,'visitexcel']);
+
+//Rekam medis
+Route::get('/showrekamedis',[AdminController::class,'showrekamedis']);
+Route::get('/detail_rekam/{id}',[AdminController::class,'detail_rekam']);
+Route::post('/editrekam/{id}',[AdminController::class,'editrekam']);
+Route::get('/showprint',[AdminController::class,'showprint']);
+Route::get('/pdf',[AdminController::class,'pdf']);
+Route::get('/rekamexcel',[AdminController::class,'rekamexcel']);
