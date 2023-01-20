a0685c80c0cc90f4a68e633cc2db9ef291b494c7
MRizkiRamdani <113973050+MRizkiRamdani@users.noreply.github.com>
2023-01-21 00:58:13 +0700

    Add files via upload

---
+++ resources/views/admin/body.blade.php
@@ -0,0 +1,59 @@
+<div class="main-panel">
+          <div class="content-wrapper">
+            <div class="row">
+              <div class="col-12 grid-margin">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Janji</h4>
+                    <div class="table-responsive">
+                      <table class="table">
+                        <thead>
+                          <tr>
+                            <th>
+                              <div class="form-check form-check-muted m-0">
+                                <label class="form-check-label">
+                                  <input type="checkbox" class="form-check-input">
+                                </label>
+                              </div>
+                            </th>
+                            <th> Nama Pasien </th>
+                            <th> Email </th>
+                            <th> Telepon </th>
+                            <th> Nama Dokter </th>
+                            <th> Tanggal </th>
+                            <th> Pesan </th>
+                            <th> Aksi </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>
+                              <div class="form-check form-check-muted m-0">
+                                <label class="form-check-label">
+                                  <input type="checkbox" class="form-check-input">
+                                </label>
+                              </div>
+                            </td>
+                            <td>
+                              <span class="pl-2"></span>
+                            </td>
+                            <td> </td>
+                            <td> </td>
+                            <td>  </td>
+                            <td>  </td>
+                            <td>  </td>
+                            
+                            <td>
+                              <div class="badge badge-outline-success">Approved</div>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+                
